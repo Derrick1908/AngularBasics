d38f0138fbbc60112fd81a5054891691d3cef600
Derrick1908 <Derrick1908>
2021-07-26 18:18:02 +0530

    Added RootScope and 2 more Controllers in a Sepaaret File

---
+++ AngularBasicApplication/Scripts/MyScripts/App.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="../angular.js" />
+var app = angular.module("myApp", []);
+
+app.controller("RedController", function ($scope, $rootScope) {
+    $scope.favColor = "Red";
+    $rootScope.color = "White";
+
+    console.log($rootScope);        ///For debugging purposes only
+})
+
+app.controller("BlueController", function ($scope) {
+    $scope.favColor = "Blue";
+})
