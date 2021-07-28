0cf65567e50eb3db777a7db4040df54419de20fe
Derrick1908 <Derrick1908>
2021-07-28 14:22:41 +0530

    Added Nested Controllers

---
+++ AngularBasicApplication/Scripts/MyScripts/AppNested.js
@@ -0,0 +1,22 @@
+﻿/// <reference path="../angular.js" />
+var app = angular.module("myApp", [])
+
+app.controller("CountryController", function ($scope) {
+    $scope.name = "India";
+    $scope.iso = "INR";
+
+    console.log($scope);        /// For Debugging Purposes only
+})
+
+app.controller("StateController", function ($scope) {
+
+    $scope.name = "Goa";
+    console.log($scope);        /// For Debugging Purposes only
+})
+
+app.controller("CityController", function ($scope) {
+
+    $scope.name = "Panjim";
+
+    console.log($scope);        /// For Debugging Purposes only
+})
