9a73acb9f9bfaa301b08287a026efcdf9be4f8d7
Derrick1908 <Derrick1908>
2021-08-13 18:07:41 +0530

    ng-src Demo

---
+++ AngularBasicApplication/Scripts/MyScripts/AppNgSrc.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="../angular.js" />
+
+angular.module("myApp", [])
+       .controller("CountryController", function ($scope) {
+                $scope.country = {
+                    name: "India",
+                    capital: "New Delhi",
+                    flag: "../Images/India.png"
+                };
+       });
